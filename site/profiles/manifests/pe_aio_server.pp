aa752735bcb42aef6a7579b9a26756192f2abf18
Eric Rumble <eric.rumble@sage.com>
2016-04-11 13:31:17 -0400

    pe can manage itself

--- site/profiles/manifests/pe_aio_server.pp
+++ site/profiles/manifests/pe_aio_server.pp
@@ -1,14 +1,5 @@
 class profiles::pe_aio_server {
 
-  class { 'puppet::server':
-    java_Xms        => '512m',
-    java_Xmx        => '512m',
-    ca              => true,
-    autosign        => true,
-    service_name    => 'pe-puppetserver',
-    manage_packages => false,
-  }
-
   class { 'hiera':
     hierarchy => [
       "nodes/%{::trusted.certname}",
@@ -21,7 +12,7 @@ class profiles::pe_aio_server {
     datadir        => '/etc/puppetlabs/code/environments/%{environment}/hieradata',
     hiera_yaml     => '/etc/puppetlabs/code/hiera.yaml',
     datadir_manage => false,
-    master_service => 'puppetserver',
+    master_service => 'pe-puppetserver',
   }
 
 }
